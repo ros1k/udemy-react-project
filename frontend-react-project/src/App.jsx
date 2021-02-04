1549c91681189804818be441051cdbd7e5da1b61
Damian <rosa.damian@gmail.com>
2021-02-04 20:03:11 +0100

    feat: display all courses

--- frontend-react-project/src/App.jsx
+++ frontend-react-project/src/App.jsx
@@ -3,7 +3,7 @@ import StoreProvider from './store/StoreProvider'
 import Header from './components/Header/Header'
 import { HashRouter as Router} from 'react-router-dom';
 import AsideMenu from './components/AsideMenu/AsideMenu'
-
+import Content from './components/Content/Content'
 import './App.scss';
 
 
@@ -13,9 +13,10 @@ const App = () => (
    <StoreProvider>
       <Header/>
       <Router>
-            <div className="content-wrapper">
-               <AsideMenu/>   
-            </div>
+         <div className="content-wrapper">
+            <AsideMenu/>   
+            <Content/>
+         </div>
       </Router>
    </StoreProvider>
   
---
+++ frontend-react-project/src/components/Content/Content.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules'
+
+import {default as ContentStyles} from './Content.module.scss'
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { StoreContext } from '../../store/StoreProvider';
+import Courses from '../Courses/Courses'
+
+const style = bemCssModules(ContentStyles)
+
+const Content = () => {
+   const {user } = useContext(StoreContext)
+
+   const isUserLogged = Boolean(user);
+   const isAdmin = user?.acceslevel === 1;
+
+   
+   return (
+      <main className={style()}>
+         <Switch>
+            <Route exact path="/" render={() => <Courses />} />
+            {isUserLogged &&  <Route exact path="/my-courses" render={() => <p> mojee kursy</p>} /> }
+            {isAdmin &&  <Route exact path="/manage-courses" render={() => <p> zarządzanie kursami</p>} /> }
+
+            <Redirect to="/"/>
+         </Switch>
+      </main>
+   )
+}
+
+export default Content
